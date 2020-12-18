c6318d6141325a05b1ced291b70f3d272f5a411e
rahi-nz <nz.rahele@gmail.com>
2020-12-18 16:09:59 +0330

    add eslint :sparkles:

---
+++ pages/assignment-1.tsx
@@ -0,0 +1,8 @@
+const Index = () => (
+    <div>
+        <h1>Technical-Assignment</h1>
+        <p>welcome</p>
+    </div>
+)
+
+export default Index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import Link from 'next/link';
 
 const Index = () => (
     <div>
         <h1>Technical-Assignment</h1>
         <p>welcome</p>
+        <ul>
+            <li>
+                <Link href={`/assignment-1`}>
+                    <a>Assignment1</a>
+                </Link>
+            </li>
+            <li>
+                <Link href={`/assignment-2`}>
+                    <a>Assignment2</a>
+                </Link>
+            </li>
+        </ul>
     </div>
 )
 
